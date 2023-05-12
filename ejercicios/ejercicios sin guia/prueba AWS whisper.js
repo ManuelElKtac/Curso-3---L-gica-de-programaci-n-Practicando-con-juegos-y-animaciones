f0010de311b0a57257f565ae87a1411fdf8a2d7b
Manuel <siguiendotujuego@gmail.com>
2023-05-12 01:30:42 -0500

    ejercicios

---
+++ ejercicios/ejercicios sin guia/prueba AWS whisper.js
@@ -0,0 +1,8 @@
+import esppres from 'espress';
+
+const app = esppres();
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+// todo eso fue escrito por el auto escrito, video de ejemplo fue: https://www.youtube.com/watch?v=fy5UUQ0aeq0
